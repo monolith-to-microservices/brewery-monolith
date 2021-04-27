bf51194b5cc1f71ecd117df04d858419191156ef
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-04-27 16:25:26 +0800

    adding circleci yaml

---
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,28 @@
+package guru.sfg.brewery.web.mappers;
+
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.web.model.BeerDto;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-04-27T16:24:07+0800",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerMapperImpl extends BeerMapperDecorator implements BeerMapper {
+
+    @Autowired
+    @Qualifier("delegate")
+    private BeerMapper delegate;
+
+    @Override
+    public Beer beerDtoToBeer(BeerDto beerDto)  {
+        return delegate.beerDtoToBeer( beerDto );
+    }
+}
---
+++ target/generated-sources/annotations/guru/sfg/brewery/web/mappers/BeerOrderLineMapperImpl.java
@@ -0,0 +1,15 @@
+package guru.sfg.brewery.web.mappers;
+
+import javax.annotation.processing.Generated;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-04-27T16:24:07+0800",
+    comments = "version: 1.3.0.Final, compiler: javac, environment: Java 15.0.2 (Oracle Corporation)"
+)
+@Component
+@Primary
+public class BeerOrderLineMapperImpl extends BeerOrderLineMapperDecorator implements BeerOrderLineMapper {
+}
